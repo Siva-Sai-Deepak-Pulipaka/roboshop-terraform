1ddb3602b406c9925ded921ab66a6a0a89cc1fb8
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-13 20:14:30 +0530

    making our project run using terraform

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,11 +1,74 @@
-resource "aws_spot_instance_request" "ec2" {
-    ami = "ami-0a017d8ceb274537d"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [ "sg-008351f2f82e44f0c" ]
-    instance_interruption_behavior = "stop"
-    tags = {
-        Name = "demo"
-    }
-    
-  
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
 }
+
+//The above data block is prerequisite for allowing to create ec2 instance
+
+resource "aws_spot_instance_request" "ec2" {
+  ami           = data.aws_ami.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  tags = {
+    Name = var.component
+  }
+
+}  
+resource "null_resource" "null" {
+    provisioner "remote-exec" {
+    connection {
+        host = aws_spot_instance_request.ec2.public_ip
+        user = "centos"
+        password = "DevOps321"
+    }
+    inline = [
+      
+      "git clone https://github.com/Siva-Sai-Deepak-Pulipaka/roboshop-shell-script.git",
+      "cd roboshop-shell-script",
+      "sudo bash ${var.component}.sh ${var.password}"
+    ]
+
+  }
+
+}
+
+resource "aws_security_group" "sg" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "Allowing all for incoming"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z017354339FNHXHAHU536"
+  name    = "${var.component}-dev.easydevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_spot_instance_request.ec2.private_ip]
+}
+
+variable "component" {}
+variable "instance_type" {}
+variable "env" {
+    default = "dev"
+}
+variable "password" {}
---
+++ vars.tf
@@ -0,0 +1,54 @@
+variable "instances" {
+    default = {
+        frontend = {
+            name = "frontend"
+            type = "t3.micro"
+        }
+        mongodb = {
+            name = "mongodb"
+            type = "t3.micro"
+        }
+        catalogue = {
+            name = "catalogue"
+            type = "t3.micro"
+        }
+        redis = {
+            name = "redis"
+            type = "t3.micro"
+        }
+        user = {
+            name = "user"
+            type = "t3.micro"
+        }
+        cart = {
+            name = "cart"
+            type = "t3.micro"
+        }
+        mysql = {
+            name = "mysql"
+            type = "t3.micro"
+            password = "RoboShop@1"
+        }
+        shipping = {
+            name = "shipping"
+            type = "t3.micro"
+            password = "RoboShop@1"
+        }
+        rabbit = {
+            name = "rabbit"
+            type = "t3.micro"
+            password = "roboshop123"
+        }
+        payment = {
+            name = "payment"
+            type = "t3.micro"
+            password = "roboshop123"
+        }
+        dispatch = {
+            name = "dispatch"
+            type = "t3.micro"
+            password = "roboshop123"
+        }
+    }
+  
+}
