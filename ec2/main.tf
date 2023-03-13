6edfe24f40571746a02352016a8aaf9028473ccc
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-14 00:47:08 +0530

    resolving errors

--- ec2/main.tf
+++ ec2/main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "ec2" {
   }
 
 }  
-resource "null_resource" "null" {
+resource "null_resource" "provisioner" {
     provisioner "remote-exec" {
     connection {
         host     = aws_instance.ec2.public_ip
