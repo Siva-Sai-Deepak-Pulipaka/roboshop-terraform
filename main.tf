6ce3cf5903fd78aad5cb2d8036de7f9156e5f9de
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-14 23:58:11 +0530

    experimenting to create spot request

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ module "ec2" {
     component     = each.value["name"]
     instance_type = each.value["type"]
     password      = try(each.value["password"], "null")
+    spot_instance_request_id = aws_spot_instance_request.ec2.id
   
 }
 //best practice is using for loop when we are dealing with module.
