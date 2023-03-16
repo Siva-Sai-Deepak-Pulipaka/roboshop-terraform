b23195f0b7a111ed36ec9848c44257c4a8c7c967
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-16 17:20:56 +0530

    experiment3 creating dependencies to install comps

--- ec2/main.tf
+++ ec2/main.tf
@@ -16,7 +16,7 @@ resource "aws_spot_instance_request" "ec2" {
 
 }  
 resource "null_resource" "provisioner" {
-  depends_on = [aws_spot_instance_request.ec2] //we have included this line because remote-exec will take some time. so we have included depends_on parameter. This will ensure you after the instance is fully created which we mentioned in depends on parameter, then only remote-exec will request to connect.  
+  depends_on = [aws_spot_instance_request.ec2[var.instances.index]] //we have included this line because remote-exec will take some time. so we have included depends_on parameter. This will ensure you after the instance is fully created which we mentioned in depends on parameter, then only remote-exec will request to connect.  
     provisioner "remote-exec" {
     connection {
         host     = aws_spot_instance_request.ec2.public_ip
@@ -72,3 +72,4 @@ variable "env" {
   default = "dev"
 }
 variable "password" {}
+variable "index" {}
