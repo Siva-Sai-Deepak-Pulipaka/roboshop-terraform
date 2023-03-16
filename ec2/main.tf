1b55c7b4ca3bb4dbc067785cd2e209abbdaffb38
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-16 20:27:57 +0530

    this works fine

--- ec2/main.tf
+++ ec2/main.tf
@@ -15,15 +15,7 @@ resource "aws_spot_instance_request" "ec2" {
   }
     
 }
-resource "aws_instance" "ec2" {
-  ami                     = data.aws_ami.ami.image_id
-  instance_type           = var.instance_type
-  vpc_security_group_ids  = [aws_security_group.sg.id]
-  tags = {
-    Name = aws_spot_instance_request.ec2.tags.Name
-  }
 
-}  
 resource "null_resource" "provisioner" {
   depends_on = [aws_spot_instance_request.ec2] //we have included this line because remote-exec will take some time. so we have included depends_on parameter. This will ensure you after the instance is fully created which we mentioned in depends on parameter, then only remote-exec will request to connect.  
     provisioner "remote-exec" {
