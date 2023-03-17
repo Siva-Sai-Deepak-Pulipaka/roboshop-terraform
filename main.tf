3bcc3d82a241414d6ff5e80521236d9d8f2fd7c6
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-17 15:01:24 +0530

    solving errors

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ module "ec2" {
     component     = each.value["name"]
     instance_type = each.value["type"]
     password      = try(each.value["password"], "null")
+    instances     = var.instances[count.index] 
     
   
 }
