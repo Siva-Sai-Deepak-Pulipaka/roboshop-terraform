69468efc3d0049ab09353959306a36ee34f797b0
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-20 14:39:40 +0530

    changes to run ansible code

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
+  name_regex       = "devops-practice-ansible"            //  we replaced The old line because we need to install ansible on every machine. so instead of doing it manually, we created a image from an instance and make it available to our account. so no need to install ansible in every system we create. 
+  owners           = [data.aws_caller_identity.current.account_id]
 }
 
 //The above data block is prerequisite for allowing to create ec2 instance
@@ -34,10 +34,8 @@ resource "null_resource" "provisioner" {
           password = "DevOps321"
     }
       inline = [
+        "ansible-pull -i localhost, -U https://github.com/Siva-Sai-Deepak-Pulipaka/roboshop-ansible.git roboshop.yml -e role_name=${var.component}"
         
-        "git clone https://github.com/Siva-Sai-Deepak-Pulipaka/roboshop-shell-script.git",
-        "cd roboshop-shell-script",
-        "sudo bash ${var.component}.sh ${var.password}"
       ]
 
   }
