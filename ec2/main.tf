528b7fb231060e923d466928ec378478362f39e8
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-20 16:33:55 +0530

    added data resource

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,3 +1,4 @@
+data "aws_caller_identity" "current" {}
 data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "devops-practice-ansible"            //  we replaced The old line because we need to install ansible on every machine. so instead of doing it manually, we created a image from an instance and make it available to our account. so no need to install ansible in every system we create. 
