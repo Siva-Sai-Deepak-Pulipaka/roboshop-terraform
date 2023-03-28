cb082ab9056c4540acebd654750a608762b0a00a
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-28 14:26:39 +0530

    forgot to add try function

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "ec2" {
     component      = each.value["name"]
     instance_type  = each.value["type"]
     env            = var.env
-    monitor        = each.value["monitor"]
+    monitor        = try(each.value["monitor"], false)
   
 }
 //best practice is using for loop when we are dealing with module.
