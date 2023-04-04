b43c1aab2f6a34a017a03022ec47ce0be8f0dfcd
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-04-04 23:08:31 +0530

    configured upto documentdb

---
+++ locals.tf
@@ -0,0 +1,7 @@
+# locals can be used as run time variables. they will be applied after terraform apply command
+locals {
+#   trying to get private subnets
+ db_subnet_ids = tolist([module.vpc["main"].private_subnets["db-az1"].id, module.vpc["main"].private_subnets["db-az2"].id])
+ app_subnet_ids = tolist([module.vpc["main"].private_subnets["app-az1"].id, module.vpc["main"].private_subnets["app-az2"].id])
+ web_subnet_ids = tolist([module.vpc["main"].private_subnets["web-az1"].id, module.vpc["main"].private_subnets["web-az2"].id])
+}
--- vars.tf
+++ vars.tf
@@ -3,4 +3,4 @@ variable "env" {}
 variable "vpc" {}
 variable "default_vpc_id" {}
 variable "default_route_table" {}
-
+variable "docdb" {}
