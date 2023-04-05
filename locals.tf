79e5acd9fd601bf49360103c26d112aa38d82c85
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-04-05 15:45:42 +0530

    changes in locals

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 # locals can be used as run time variables. they will be applied after terraform apply command
 locals {
 #   trying to get private subnets
- db_subnet_ids = tolist([module.vpc["main"].private_subnets["db-az1"].id, module.vpc["main"].private_subnets["db-az2"].id])
- app_subnet_ids = tolist([module.vpc["main"].private_subnets["app-az1"].id, module.vpc["main"].private_subnets["app-az2"].id])
- web_subnet_ids = tolist([module.vpc["main"].private_subnets["web-az1"].id, module.vpc["main"].private_subnets["web-az2"].id])
+ db_subnet_ids = tolist([module.vpc["main"].private_subnets["db-azone1"].id, module.vpc["main"].private_subnets["db-azone2"].id])
+ app_subnet_ids = tolist([module.vpc["main"].private_subnets["app-azone1"].id, module.vpc["main"].private_subnets["app-azone2"].id])
+ web_subnet_ids = tolist([module.vpc["main"].private_subnets["web-azone1"].id, module.vpc["main"].private_subnets["web-azone2"].id])
 }
