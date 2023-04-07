f6fa7ae435e14494ac6968b89bc77fcf82c96ad7
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-04-07 14:09:03 +0530

    debug

--- main.tf
+++ main.tf
@@ -11,71 +11,71 @@ module "vpc" {
   private_subnets= each.value["private_subnets"]
 }
 
-module "docdb" {
-  source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-docdb-module.git"
-  env    = var.env
-  tags   = var.tags
+# module "docdb" {
+#   source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-docdb-module.git"
+#   env    = var.env
+#   tags   = var.tags
   
-  subnet_ids              = local.db_subnet_ids
-  # vpc_id                  = module.vpc["main"].vpc_id
+#   subnet_ids              = local.db_subnet_ids
+#   # vpc_id                  = module.vpc["main"].vpc_id
 
-  for_each                = var.docdb
-  engine                  = each.value["engine"]
-  engine_version          = each.value["engine_version"]
-  backup_retention_period = each.value["backup_retention_period"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot     = each.value["skip_final_snapshot"]
-  no_of_instances         = each.value["no_of_instances"]
-  instance_class          = each.value["instance_class"]
-  # allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
+#   for_each                = var.docdb
+#   engine                  = each.value["engine"]
+#   engine_version          = each.value["engine_version"]
+#   backup_retention_period = each.value["backup_retention_period"]
+#   preferred_backup_window = each.value["preferred_backup_window"]
+#   skip_final_snapshot     = each.value["skip_final_snapshot"]
+#   no_of_instances         = each.value["no_of_instances"]
+#   instance_class          = each.value["instance_class"]
+#   # allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
 
-}
+# }
 
-module "rds" {
-  source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-rds-module.git"
-  env    = var.env
-  tags   = var.tags
+# module "rds" {
+#   source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-rds-module.git"
+#   env    = var.env
+#   tags   = var.tags
  
-  subnet_ids              = local.db_subnet_ids
+#   subnet_ids              = local.db_subnet_ids
 
-  for_each                = var.rds
-  engine                  = each.value["engine"]
-  engine_version          = each.value["engine_version"]
-  backup_retention_period = each.value["backup_retention_period"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot     = each.value["skip_final_snapshot"]
-  no_of_instances         = each.value["no_of_instances"]
-  instance_class          = each.value["instance_class"]
+#   for_each                = var.rds
+#   engine                  = each.value["engine"]
+#   engine_version          = each.value["engine_version"]
+#   backup_retention_period = each.value["backup_retention_period"]
+#   preferred_backup_window = each.value["preferred_backup_window"]
+#   skip_final_snapshot     = each.value["skip_final_snapshot"]
+#   no_of_instances         = each.value["no_of_instances"]
+#   instance_class          = each.value["instance_class"]
 
-}
+# }
 
-module "elasticache" {
-  source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-elasticache-module.git"
-  env    = var.env
-  tags   = var.tags
+# module "elasticache" {
+#   source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-elasticache-module.git"
+#   env    = var.env
+#   tags   = var.tags
   
-  subnet_ids = local.db_subnet_ids
+#   subnet_ids = local.db_subnet_ids
 
-  for_each                = var.elasticache
-  engine                  = each.value["engine"]
-  engine_version          = each.value["engine_version"]
-  num_cache_nodes         = each.value["num_cache_nodes"]
-  node_type               = each.value["node_type"]
+#   for_each                = var.elasticache
+#   engine                  = each.value["engine"]
+#   engine_version          = each.value["engine_version"]
+#   num_cache_nodes         = each.value["num_cache_nodes"]
+#   node_type               = each.value["node_type"]
  
 
-}
+# }
 
-module "rabbitmq" {
-  source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-rabbitmq-module.git"
-  env    = var.env
-  tags   = var.tags
+# module "rabbitmq" {
+#   source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-rabbitmq-module.git"
+#   env    = var.env
+#   tags   = var.tags
   
-  subnet_ids = local.db_subnet_ids
+#   subnet_ids = local.db_subnet_ids
   
 
-  for_each                = var.rabbitmq
-  instance_type           = each.value["instance_type"]
-}
+#   for_each                = var.rabbitmq
+#   instance_type           = each.value["instance_type"]
+# }
 
 module "alb" {
   source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-alb-module.git"
