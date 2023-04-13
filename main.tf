6c70edc9e59b21a4f46075b1aac20ef38ce5904f
deepak <18l11a0518@gmail.com>
2023-04-13 17:27:00 +0530

    commented all db services

--- main.tf
+++ main.tf
@@ -11,79 +11,79 @@ module "vpc" {
   private_subnets= each.value["private_subnets"]
 }
 
-module "docdb" {
-  source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-docdb-module.git"
-  env    = var.env
-  tags   = var.tags
+# module "docdb" {
+#   source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-docdb-module.git"
+#   env    = var.env
+#   tags   = var.tags
   
-  subnet_ids  = local.db_subnet_ids
-  vpc_id      = module.vpc["main"].vpc_id
+#   subnet_ids  = local.db_subnet_ids
+#   vpc_id      = module.vpc["main"].vpc_id
   
-  for_each                = var.docdb
-  engine                  = each.value["engine"]
-  engine_version          = each.value["engine_version"]
-  backup_retention_period = each.value["backup_retention_period"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot     = each.value["skip_final_snapshot"]
-  no_of_instances         = each.value["no_of_instances"]
-  instance_class          = each.value["instance_class"]
-  allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
+#   for_each                = var.docdb
+#   engine                  = each.value["engine"]
+#   engine_version          = each.value["engine_version"]
+#   backup_retention_period = each.value["backup_retention_period"]
+#   preferred_backup_window = each.value["preferred_backup_window"]
+#   skip_final_snapshot     = each.value["skip_final_snapshot"]
+#   no_of_instances         = each.value["no_of_instances"]
+#   instance_class          = each.value["instance_class"]
+#   allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
 
-}
+# }
 
-module "rds" {
-  source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-rds-module.git"
-  env    = var.env
-  tags   = var.tags
+# module "rds" {
+#   source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-rds-module.git"
+#   env    = var.env
+#   tags   = var.tags
  
-  subnet_ids              = local.db_subnet_ids
-  vpc_id      = module.vpc["main"].vpc_id
+#   subnet_ids              = local.db_subnet_ids
+#   vpc_id      = module.vpc["main"].vpc_id
 
-  for_each                = var.rds
-  engine                  = each.value["engine"]
-  engine_version          = each.value["engine_version"]
-  backup_retention_period = each.value["backup_retention_period"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot     = each.value["skip_final_snapshot"]
-  no_of_instances         = each.value["no_of_instances"]
-  instance_class          = each.value["instance_class"]
-  allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
+#   for_each                = var.rds
+#   engine                  = each.value["engine"]
+#   engine_version          = each.value["engine_version"]
+#   backup_retention_period = each.value["backup_retention_period"]
+#   preferred_backup_window = each.value["preferred_backup_window"]
+#   skip_final_snapshot     = each.value["skip_final_snapshot"]
+#   no_of_instances         = each.value["no_of_instances"]
+#   instance_class          = each.value["instance_class"]
+#   allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
 
-}
+# }
 
-module "elasticache" {
-  source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-elasticache-module.git"
-  env    = var.env
-  tags   = var.tags
+# module "elasticache" {
+#   source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-elasticache-module.git"
+#   env    = var.env
+#   tags   = var.tags
   
-  subnet_ids = local.db_subnet_ids
-  vpc_id      = module.vpc["main"].vpc_id
+#   subnet_ids = local.db_subnet_ids
+#   vpc_id      = module.vpc["main"].vpc_id
 
-  for_each                = var.elasticache
-  engine                  = each.value["engine"]
-  engine_version          = each.value["engine_version"]
-  num_cache_nodes         = each.value["num_cache_nodes"]
-  node_type               = each.value["node_type"]
-  allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
+#   for_each                = var.elasticache
+#   engine                  = each.value["engine"]
+#   engine_version          = each.value["engine_version"]
+#   num_cache_nodes         = each.value["num_cache_nodes"]
+#   node_type               = each.value["node_type"]
+#   allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
  
 
-}
+# }
 
-module "rabbitmq" {
-  source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-rabbitmq-module.git"
-  env    = var.env
-  tags   = var.tags
-  bastion_cidr = var.bastion_cidr
-  dns_domain   = var.dns_domain
+# module "rabbitmq" {
+#   source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-rabbitmq-module.git"
+#   env    = var.env
+#   tags   = var.tags
+#   bastion_cidr = var.bastion_cidr
+#   dns_domain   = var.dns_domain
   
-  subnet_ids = local.db_subnet_ids
-  vpc_id      = module.vpc["main"].vpc_id
+#   subnet_ids = local.db_subnet_ids
+#   vpc_id      = module.vpc["main"].vpc_id
   
 
-  for_each                = var.rabbitmq
-  instance_type           = each.value["instance_type"]
-  allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
-}
+#   for_each                = var.rabbitmq
+#   instance_type           = each.value["instance_type"]
+#   allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
+# }
 
 module "alb" {
   source = "git::https://github.com/Siva-Sai-Deepak-Pulipaka/terraform-alb-module.git"
@@ -140,41 +140,41 @@ output "alb" {
 # load generator 
 
 
-resource "aws_spot_instance_request" "load-generator" {
-  ami                  = data.aws_ami.ami.id
-  instance_type        = "t3.medium"
-  wait_for_fulfillment = true
-  vpc_security_group_ids = ["sg-008351f2f82e44f0c"]   #we are giving our allow all vpcid because it will give public ip
+# resource "aws_spot_instance_request" "load-generator" {
+#   ami                  = data.aws_ami.ami.id
+#   instance_type        = "t3.medium"
+#   wait_for_fulfillment = true
+#   vpc_security_group_ids = ["sg-008351f2f82e44f0c"]   #we are giving our allow all vpcid because it will give public ip
 
 
-  tags = merge(
-    var.tags,
-    { Name = "load-generator" }
-  )
-}
-resource "aws_ec2_tag" "tag" {
-  key = "Name"
-  resource_id = aws_spot_instance_request.load-generator.spot_instance_id
-  value = "load-generator"
-}
+#   tags = merge(
+#     var.tags,
+#     { Name = "load-generator" }
+#   )
+# }
+# resource "aws_ec2_tag" "tag" {
+#   key = "Name"
+#   resource_id = aws_spot_instance_request.load-generator.spot_instance_id
+#   value = "load-generator"
+# }
 
-# load generator can be automated by the below code
+# # load generator can be automated by the below code
 
-resource "null_resource" "load-gen" {
-  triggers = {
-    abc = aws_spot_instance_request.load-generator.public_ip
-  }
-  provisioner "remote-exec" {
-    connection {
-      host = aws_spot_instance_request.load-generator.public_ip
-      user = "root"
-      password = data.aws_ssm_parameter.ssh-pass.value
-    }
-    inline = [
-      "curl -s -L https://get.docker.com | bash",
-      "systemctl enable docker",
-      "systemctl start docker",
-      "docker pull robotshop/rs-load"
-    ]
-  }
-}
+# resource "null_resource" "load-gen" {
+#   triggers = {
+#     abc = aws_spot_instance_request.load-generator.public_ip
+#   }
+#   provisioner "remote-exec" {
+#     connection {
+#       host = aws_spot_instance_request.load-generator.public_ip
+#       user = "root"
+#       password = data.aws_ssm_parameter.ssh-pass.value
+#     }
+#     inline = [
+#       "curl -s -L https://get.docker.com | bash",
+#       "systemctl enable docker",
+#       "systemctl start docker",
+#       "docker pull robotshop/rs-load"
+#     ]
+#   }
+# }
