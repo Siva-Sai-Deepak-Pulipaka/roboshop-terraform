2a4851c5055b18ddc47ffb43dae37e36dbff3f4e
deepak <18l11a0518@gmail.com>
2023-04-28 14:29:06 +0530

    we are using centos 7 ami from aws marketplace

--- data.tf
+++ data.tf
@@ -1,8 +1,8 @@
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex = "devops-practice-ansible"
-  owners = ["self"]
-}
+# data "aws_ami" "ami" {
+#   most_recent = true
+#   name_regex = "devops-practice-ansible"
+#   owners = ["self"]
+# }
 
 
 
