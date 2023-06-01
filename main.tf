faaa7ceac39bc1e0a1a18c985c63a980dba608f9
deepak <18l11a0518@gmail.com>
2023-06-02 02:01:34 +0530

    shifted from minikube to EKS

--- main.tf
+++ main.tf
@@ -179,7 +179,7 @@ module "vpc" {
 #   }
 # }
 
-# minikube configuration
+/* minikube configuration
 module "minikube" {
   source = "github.com/scholzj/terraform-aws-minikube"
   aws_region          = "us-east-1"
@@ -204,4 +204,16 @@ output "MINIKUBE_SERVER" {
 }
 output "KUBE_CONFIG" {
   value = "scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig ~/.kube/config"
+}
+
+*/
+
+module "eks" {
+  source             = "github.com/r-devops/tf-module-eks"
+  ENV                = var.env
+  PRIVATE_SUBNET_IDS = lookup(local.subnet_ids, "app", null)
+  PUBLIC_SUBNET_IDS  = lookup(local.subnet_ids, "public", null)
+  DESIRED_SIZE       = 2
+  MAX_SIZE           = 2
+  MIN_SIZE           = 2
 }
